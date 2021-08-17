7eb9f3ec88e6f143c92832a8aba0374a57f46282
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-08-17 20:32:16 +0200

    Add files via upload

---
+++ Kookaburra.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Kookaburra.SDK;
+using System.Threading;
+using Kookaburra.SDK.Networking;
+
+namespace Kookaburra.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug test = new Debug(@"D:\Projects\Kookaburra\publish\win-x64\KookaburraShell.exe");
+            test.Start(true);
+            test.Stop();
+        }
+    }
+}
