7062efe5a0afd5f37e01b5118a54c7403a8c42ed
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-08-18 13:25:42 +0200

    Add files via upload

---
+++ Kookaburra.SDK/Env.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kookaburra.SDK
+{
+    /// <summary>
+    /// Environment Class for the Kookaburra SDK.
+    /// </summary>
+    public static class Env
+    {
+        /// <summary>
+        /// If True, the system is x64 based.
+        /// </summary>
+        public static readonly bool Is64x = Environment.Is64BitProcess;
+        /// <summary>
+        /// If True, the running program is x64 based.
+        /// </summary>
+        public static readonly bool Is64xProcess = Environment.Is64BitOperatingSystem;
+        /// <summary>
+        /// Returns the current OS Platform.
+        /// </summary>
+        public static readonly string OSPlatform = Environment.OSVersion.Platform.ToString();
+        /// <summary>
+        /// Returns the current machinename.
+        /// </summary>
+        public static readonly string MachineName = Environment.MachineName;
+        /// <summary>
+        /// Returns the current username.
+        /// </summary>
+        public static readonly string UserName = Environment.UserName;
+        /// <summary>
+        /// Returns the current working directory.
+        /// </summary>
+        public static readonly string CurrentDirectory = Environment.CurrentDirectory;
+    }
+}
