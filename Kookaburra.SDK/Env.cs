a2ac925266da3bf2fecc4e3d031375debdd34a3e
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-08-18 18:47:30 +0200

    Updated to `0.0.2`.

--- Kookaburra.SDK/Env.cs
+++ Kookaburra.SDK/Env.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 using System.Text;
+using System.Management;
 using System.Threading.Tasks;
 
 namespace Kookaburra.SDK
@@ -16,25 +19,196 @@ namespace Kookaburra.SDK
         /// If True, the system is x64 based.
         /// </summary>
         public static readonly bool Is64x = Environment.Is64BitProcess;
+        
         /// <summary>
         /// If True, the running program is x64 based.
         /// </summary>
         public static readonly bool Is64xProcess = Environment.Is64BitOperatingSystem;
+        
         /// <summary>
         /// Returns the current OS Platform.
         /// </summary>
-        public static readonly string OSPlatform = Environment.OSVersion.Platform.ToString();
+        public static string GetOSPlatform()
+        {
+            return Environment.OSVersion.Platform.ToString();
+        }
+
         /// <summary>
         /// Returns the current machinename.
         /// </summary>
         public static readonly string MachineName = Environment.MachineName;
+        
         /// <summary>
         /// Returns the current username.
         /// </summary>
         public static readonly string UserName = Environment.UserName;
+        
         /// <summary>
         /// Returns the current working directory.
         /// </summary>
         public static readonly string CurrentDirectory = Environment.CurrentDirectory;
+
+        [SupportedOSPlatform("windows")]
+        [DllImport("shell32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsUserAnAdmin();
+        [SupportedOSPlatform("windows")]
+        /// <summary>
+        /// Returns the current program is running in administrator mode.
+        /// </summary>
+        public static bool IsCurrentProcessAdmin()
+        {
+            return IsUserAnAdmin();
+        }
+
+        [SupportedOSPlatform("windows")]
+        /// <summary>
+        /// Returns the manufacturer name of the current computer.
+        /// </summary>
+        public static string GetManufacturerName() 
+        {
+            string Name = "";
+            ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+            //collection to store all management objects
+            ManagementObjectCollection moc = mc.GetInstances();
+            if (moc.Count != 0)
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    // display general system information
+                    Name = mo["Manufacturer"].ToString();
+                }
+            }
+            return Name;
+        }
+
+        [SupportedOSPlatform("windows")]
+        /// <summary>
+        /// Returns the CPU status. (using Win32_Processor API)
+        /// </summary>
+        public static string GetCPUStatus()
+        {
+            string Name = "";
+            ManagementClass mc = new ManagementClass("Win32_Processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+            if (moc.Count != 0)
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    // display general system information
+                    Name = mo["Status"].ToString();
+                }
+            }
+            return Name;
+        }
+
+        [SupportedOSPlatform("windows")]
+        /// <summary>
+        /// Returns the CPU caption. (using Win32_Processor API)
+        /// </summary>
+        public static string GetCPUCaption()
+        {
+            string Name = "";
+            ManagementClass mc = new ManagementClass("Win32_Processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+            if (moc.Count != 0)
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    // display general system information
+                    Name = mo["Caption"].ToString();
+                }
+            }
+            return Name;
+        }
+
+        [SupportedOSPlatform("windows")]
+        /// <summary>
+        /// Returns the GPU caption. (using Win32_VideoController API)
+        /// </summary>
+        public static string GetGPUCaption()
+        {
+            string Name = "";
+            ManagementClass mc = new ManagementClass("Win32_VideoController");
+            ManagementObjectCollection moc = mc.GetInstances();
+            if (moc.Count != 0)
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    // display general system information
+                    Name = mo["Caption"].ToString();
+                }
+            }
+            return Name;
+        }
+
+        [SupportedOSPlatform("windows")]
+        /// <summary>
+        /// Returns the local drive caption. (using Win32_VideoController API)
+        /// </summary>
+        public static string GetLocalDriveCaption()
+        {
+            string Name = "";
+            ManagementClass mc = new ManagementClass("Win32_LogicalDisk");
+            ManagementObjectCollection moc = mc.GetInstances();
+            if (moc.Count != 0)
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    // display general system information
+                    Name = mo["Caption"].ToString();
+                }
+            }
+            return Name;
+        }
+
+        [SupportedOSPlatform("windows")]
+        /// <summary>
+        /// Returns the requested data from the Win32 API.
+        /// </summary>
+        public static string GetWin32Info(string Class, string Instance)
+        {
+            string Name = "";
+            ManagementClass mc = new ManagementClass(Class);
+            ManagementObjectCollection moc = mc.GetInstances();
+            if (moc.Count != 0)
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    // display general system information
+                    Name = mo[Instance].ToString();
+                }
+            }
+            return Name;
+        }
+
+        /// <summary>
+        /// Returns the current Operating System. (Windows, Linux, MacOS, FreeBSD)
+        /// </summary>
+        public static string GetOSType() 
+        {
+            string OS = "";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                OS = "Windows";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                OS = "Linux";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                OS = "MacOS";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
+            {
+                OS = "FreeBSD";
+            }
+            else 
+            {
+                throw new Exception("Invalid OS.");
+            }
+            return OS;
+        }
     }
 }
--- Kookaburra.SDK/Networking.cs
+++ Kookaburra.SDK/Networking.cs
@@ -1,9 +1,65 @@
-﻿using System.Net.NetworkInformation;
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
-namespace Kookaburra.SDK.Networking
+namespace Kookaburra.SDK
 {
+    /// <summary>
+    /// Networking Class for the Kookaburra SDK.
+    /// </summary>
     public static class Networking
     {
-        public static bool HasInternet = NetworkInterface.GetIsNetworkAvailable(); //test
+        /// <summary>
+        /// If True, the current PC has an internet connection.
+        /// </summary>
+        public static readonly bool HasInternet = NetworkInterface.GetIsNetworkAvailable();
+        /// <summary>
+        /// Returns the local IP of the current system.
+        /// </summary>
+        public static string GetLocalIP() 
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address found.");
+        }
+        /// <summary>
+        /// Returns the public IP of the current system using the 'checkip.dydns.org' API.
+        /// </summary>
+        public static string GetPublicIP() 
+        {
+            string url = "http://checkip.dyndns.org";
+            System.Net.WebRequest req = System.Net.WebRequest.Create(url);
+            System.Net.WebResponse resp = req.GetResponse();
+            System.IO.StreamReader sr = new System.IO.StreamReader(resp.GetResponseStream());
+            string response = sr.ReadToEnd().Trim();
+            string[] a = response.Split(':');
+            string a2 = a[1].Substring(1);
+            string[] a3 = a2.Split('<');
+            string a4 = a3[0];
+            return a4;
+        }
+        /// <summary>
+        /// Downloads the mentioned file to the mentioned address, using the System.Net.DownloadFile API.
+        /// </summary>
+        public static void Download(string Address, string FileLocation) 
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile(Address, FileLocation);
+        }
+        /// <summary>
+        /// Downloads the mentioned file to the mentioned address asynchronous, using the System.Net.DownloadFileAsync API.
+        /// </summary>
+        public static void DownloadAsync(string Address, string FileLocation)
+        {
+            WebClient client = new WebClient();
+            client.DownloadFileAsync(new System.Uri(Address), FileLocation);
+        }
     }
 }
